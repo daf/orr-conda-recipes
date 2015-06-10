18814e1b08b81641c0e10856ca3189e0ca0d1953
chris.barker <chris.barker@noaa.gov>
2015-06-10 10:07:34 -0700

    added a recipe for wxPython -- may not work yet...

---
+++ wxpython/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## build script tested on OS-X
+
+PYTHON=python2.7
+
+cd wxPython
+$PYTHON build-wxpython.py --build_dir=../bld --osx_cocoa
+
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
